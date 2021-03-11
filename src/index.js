511512b4a5e3a422a8e65ee69931e156bdce02a7
Fabi <frontfabi@gmail.com>
2021-03-11 20:11:49 -0300

    [del] samples

--- src/index.js
+++ src/index.js
@@ -2,13 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { GithubContextProvider } from './contexts'
 
 ReactDOM.render(
   <React.StrictMode>
-    <GithubContextProvider>
-      <App />
-    </GithubContextProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
